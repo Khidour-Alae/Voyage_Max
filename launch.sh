d0a776418015046274b982598dbc6ea886471736
Khidour-Alae <106739586+Khidour-Alae@users.noreply.github.com>
2024-02-18 01:31:37 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,7 +4,6 @@
 docker rm -f mon_conteneur_tgvmax_app
 
 # Lancer un nouveau conteneur à partir de l'image tgvmax_app
-# Assurez-vous que le port et le nom de l'image correspondent à vos configurations
 docker run -d -p 8050:8050 --name mon_conteneur_tgvmax_app tgvmax_app:latest
 
 # Vérifier si le conteneur démarre correctement
